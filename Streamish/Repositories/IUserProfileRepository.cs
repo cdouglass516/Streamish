08ede6c3750aaee50696639c2b1aa60a86c91762
Chris Douglass <cdouglass516@gmail.com>
2021-07-07 13:26:59 -0500

    first commit

---
+++ Streamish/Repositories/IUserProfileRepository.cs
@@ -0,0 +1,14 @@
+﻿using Streamish.Models;
+using System.Collections.Generic;
+
+namespace Streamish.Repositories
+{
+    public interface IUserProfileRepository
+    {
+        void Add(UserProfile user);
+        void Delete(int id);
+        UserProfile Edit(UserProfile user);
+        List<UserProfile> GetAll();
+        UserProfile GetById(int id);
+    }
+}
---
+++ Streamish/Repositories/UserProfileRepository.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+using Streamish.Models;
+using Streamish.Utils;
+
+namespace Streamish.Repositories
+{
+    public class UserProfileRepository : BaseRepository, IUserProfileRepository
+    {
+        public UserProfileRepository(IConfiguration configuration) : base(configuration) { }
+
+        public List<UserProfile> GetAll()
+        {
+            return new List<UserProfile>();
+        }
+
+        public UserProfile GetById(int id)
+        {
+            return new UserProfile();
+        }
+
+        public void Delete(int id)
+        {
+
+        }
+
+        public void Add(UserProfile user)
+        {
+
+        }
+
+        public UserProfile Edit(UserProfile user)
+        {
+            return new UserProfile();
+        }
+    }
+}
